0b4992d2f9ff089237e2b5ed5157cd1fb2157432
AhsanKhan47 <ahsankhanafridi2000@gmail.com>
2024-10-10 11:05:15 +0500

    animation styles added

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,3 +1,44 @@
+// import type { Config } from "tailwindcss";
+
+// const config: Config = {
+//   content: [
+//     "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
+//     "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
+//     "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
+//   ],
+//   theme: {
+//     extend: {
+//       screens: {
+//         'sm': '320px',
+//         'mm': '375px',
+//         'ml': '425px',
+//         'md': '768px',
+//         'xmd': '831px',
+//         'lg': '1024px',
+//         'xlg': '1150px',
+//         'xl': '1440px',
+//         '2xl': '2560px',
+//       },
+//       colors: {
+//         background: "var(--background)",
+//         foreground: "var(--foreground)",
+//       },
+//       keyframes: {
+//         'fade-in-out': {
+//           '0%': { opacity: 0 },
+//           '20%': { opacity: 1 },
+//           '80%': { opacity: 1 },
+//           '100%': { opacity: 0 },
+//         }
+//       },
+//       animation: {
+//         'fade-in-out': 'fade-in-out 5s ease-in-out infinite', // Adjust the timing here as needed
+//       },
+//     },
+//   },
+//   plugins: [],
+// };
+// export default config;
 import type { Config } from "tailwindcss";
 
 const config: Config = {
@@ -23,8 +64,20 @@ const config: Config = {
         background: "var(--background)",
         foreground: "var(--foreground)",
       },
+      keyframes: {
+        'fade-in-out': {
+          '0%': { opacity: 0 },
+          '20%': { opacity: 1 },
+          '80%': { opacity: 1 },
+          '100%': { opacity: 0 },
+        },
+      },
+      animation: {
+        'fade-in-out': 'fade-in-out 5s ease-in-out infinite', // Wrap '5s' in quotes to avoid the type error
+      },
     },
   },
   plugins: [],
 };
+
 export default config;
